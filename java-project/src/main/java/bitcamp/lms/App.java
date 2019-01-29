1c806206bb4d3b9633c9f2638ba49f46cb4a56c7
HANSEONGWOO <ghost5409@naver.com>
2019-01-29 20:45:48 +0900

    okay

--- java-project/src/main/java/bitcamp/lms/App.java
+++ java-project/src/main/java/bitcamp/lms/App.java
@@ -8,15 +8,25 @@ import java.util.Queue;
 import java.util.Scanner;
 import java.util.Stack;
 import bitcamp.lms.domain.Board;
+import bitcamp.lms.domain.Lesson;
+import bitcamp.lms.domain.Member;
 import bitcamp.lms.handler.BoardAddCommand;
 import bitcamp.lms.handler.BoardDeleteCommand;
 import bitcamp.lms.handler.BoardDetailCommand;
-import bitcamp.lms.handler.BoardHandler;
 import bitcamp.lms.handler.BoardListCommand;
 import bitcamp.lms.handler.BoardUpdateCommand;
 import bitcamp.lms.handler.Command;
-import bitcamp.lms.handler.LessonHandler;
-import bitcamp.lms.handler.MemberHandler;
+import bitcamp.lms.handler.LessonAddCommand;
+import bitcamp.lms.handler.LessonDeleteCommand;
+import bitcamp.lms.handler.LessonDetailCommand;
+import bitcamp.lms.handler.LessonListCommand;
+import bitcamp.lms.handler.LessonUpdateCommand;
+import bitcamp.lms.handler.MemberAddCommand;
+import bitcamp.lms.handler.MemberDeleteCommand;
+import bitcamp.lms.handler.MemberDetailCommand;
+import bitcamp.lms.handler.MemberListCommand;
+import bitcamp.lms.handler.MemberUpdateCommand;
+
 
 public class App {
 
@@ -28,17 +38,30 @@ public class App {
 
   public static void main(String[] args) {
     HashMap<String, Command> CommandMap = new HashMap<>();
-    
-    LessonHandler lessonHandler = new LessonHandler(keyboard, new ArrayList<>());
-    MemberHandler memberHandler = new MemberHandler(keyboard, new LinkedList<>());
+    ArrayList<Lesson> lessonlist = new ArrayList<>();
+    ArrayList<Member> memberlist = new ArrayList<>();
     ArrayList<Board> boardlist = new ArrayList<>();
-    BoardHandler boardHandler2 = new BoardHandler(keyboard, new LinkedList<>());
+
+
+    CommandMap.put("/lesson/add", new LessonAddCommand(keyboard, lessonlist));
+    CommandMap.put("/lesson/list", new LessonListCommand(keyboard, lessonlist));
+    CommandMap.put("/lesson/detail", new LessonDetailCommand(keyboard, lessonlist));
+    CommandMap.put("/lesson/update", new LessonUpdateCommand(keyboard, lessonlist));
+    CommandMap.put("/lesson/delete", new LessonDeleteCommand(keyboard, lessonlist));
+
+    CommandMap.put("/member/add", new MemberAddCommand(keyboard, memberlist));
+    CommandMap.put("/member/list", new MemberListCommand(keyboard, memberlist));
+    CommandMap.put("/member/detail", new MemberDetailCommand(keyboard, memberlist));
+    CommandMap.put("/member/update", new MemberUpdateCommand(keyboard, memberlist));
+    CommandMap.put("/member/delete", new MemberDeleteCommand(keyboard, memberlist));
 
     CommandMap.put("/board/add", new BoardAddCommand(keyboard, boardlist));
     CommandMap.put("/board/list", new BoardListCommand(keyboard, boardlist));
     CommandMap.put("/board/detail", new BoardDetailCommand(keyboard, boardlist));
     CommandMap.put("/board/update", new BoardUpdateCommand(keyboard, boardlist));
     CommandMap.put("/board/delete", new BoardDeleteCommand(keyboard, boardlist));
+
+
 
     while (true) {
       String command = prompt();
@@ -60,20 +83,20 @@ public class App {
           public boolean hasNext() {
             return index >= 0;
           }
+
           @Override
           public String next() {
             return commandHistory.get(index--);
           }
         });
-      }
-       else if (command.equals("history2")) {
+      } else if (command.equals("history2")) {
         printCommandHistory(commandHistory2.iterator());
       } else {
-           Command commandHandler = CommandMap.get(command);
-     if(commandHandler == null) 
-        System.out.println("실행할 수 없는 명령입니다.");
-     else
-       commandHandler.execute();
+        Command commandHandler = CommandMap.get(command);
+        if (commandHandler == null)
+          System.out.println("실행할 수 없는 명령입니다.");
+        else
+          commandHandler.execute();
       }
       System.out.println();
     }
@@ -98,9 +121,8 @@ public class App {
     }
   }
 
-  private static String prompt(){
+  private static String prompt() {
     System.out.print("명령> ");
     return keyboard.nextLine().toLowerCase();
   }
 }
-
