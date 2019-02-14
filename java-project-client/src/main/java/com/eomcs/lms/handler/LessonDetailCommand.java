38519494b2d51d537a884557e20464344d81e9fb
HANSEONGWOO <ghost5409@naver.com>
2019-02-14 20:42:12 +0900

    okay

--- java-project-client/src/main/java/com/eomcs/lms/handler/LessonDetailCommand.java
+++ java-project-client/src/main/java/com/eomcs/lms/handler/LessonDetailCommand.java
@@ -1,53 +1,33 @@
 package com.eomcs.lms.handler;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.util.List;
 import java.util.Scanner;
+import com.eomcs.lms.agent.LessonAgent;
 import com.eomcs.lms.domain.Lesson;
 
 public class LessonDetailCommand implements Command {
-
+  LessonAgent lessonAgent ;
   Scanner keyboard;
 
-
-  public LessonDetailCommand(Scanner keyboard) {
+  public LessonDetailCommand(Scanner keyboard, LessonAgent lessonAgent ) {
     this.keyboard = keyboard;
-    
+    this.lessonAgent = lessonAgent;
   }
 
   @Override
-  public void execute(ObjectInputStream in, ObjectOutputStream out) {
+  public void execute() {
     System.out.print("번호? ");
     int no = Integer.parseInt(keyboard.nextLine());
-    
-   try {
-     out.writeUTF("/lesson/detail");
-     out.flush();
-     if (!in.readUTF().equals("OK"))
-       throw new Exception("서버에서 해당 명령어를 처리하지 못합니다.");
-     
-     out.writeInt(no);
-     out.flush();
-     
-     String status = in.readUTF();
-     
-     if (!status.equals("OK")) 
-       throw new Exception("서버에서 게시글 목록 가져오기 실패!");
+
+    try {
+      Lesson lesson = lessonAgent.get(no);
+      System.out.printf("수업명: %s\n", lesson.getTitle());
+      System.out.printf("설명: %s\n", lesson.getContents());
+      System.out.printf("기간: %s ~ %s\n", lesson.getStartDate(), lesson.getEndDate());
+      System.out.printf("총수업시간: %d\n", lesson.getTotalHours());
+      System.out.printf("일수업시간: %d\n", lesson.getDayHours());
       
-     
-     Lesson lesson = (Lesson) in.readObject();
-     System.out.printf("수업명: %s\n", lesson.getTitle());
-     System.out.printf("설명: %s\n", lesson.getContents());
-     System.out.printf("기간: %s ~ %s\n", lesson.getStartDate(), lesson.getEndDate());
-     System.out.printf("총수업시간: %d\n", lesson.getTotalHours());
-     System.out.printf("일수업시간: %d\n", lesson.getDayHours());
     } catch (Exception e) {
-      System.out.printf("게시글 상세 정보 출력 오류! : %s\n", e.getMessage());
+      System.out.printf("실행 오류! : %s\n", e.getMessage());
     }
 
-
   }
-
-  
-  }
-
+}
