bcc1b7106d13ecec25126cc9c6d06e42b354e2ad
HANSEONGWOO <ghost5409@naver.com>
2019-03-22 18:49:54 +0900

    okay

---
+++ java-project2-server/src25/main/java/com/eomcs/lms/AppConfig.java
@@ -0,0 +1,16 @@
+package com.eomcs.lms;
+
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan(basePackages="com.eomcs.lms")
+public class AppConfig {
+
+
+  
+}
+
+
+
+
+
+
